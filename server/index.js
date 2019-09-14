67b7ce35329401ef42d4adb3eab5d3355c242e37
Glenn Reyes <glenn@glennreyes.com>
2019-09-14 11:29:37 +0200

    Return server progress to zero

--- server/index.js
+++ server/index.js
@@ -1,15 +1,1 @@
-const { ApolloServer, gql } = require('apollo-server');
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
-
-const server = new ApolloServer({ resolvers, typeDefs });
+console.log('It works!');
