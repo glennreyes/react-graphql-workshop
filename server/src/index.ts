8fc0996c64ff5c75f8b082cda72f25107294f98f
Glenn Reyes <glenn@glennreyes.com>
2023-09-25 13:52:37 +0200

    Add `graphql-yoga`

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'node:http';
+import { createSchema, createYoga } from 'graphql-yoga';
+
+const yoga = createYoga({
+  schema: createSchema({
+    resolvers: {
+      Query: {
+        hello: () => 'Hello from Yoga!',
+      },
+    },
+    typeDefs: /* GraphQL */ `
+      type Query {
+        hello: String
+      }
+    `,
+  }),
+});
+
+const server = createServer(yoga);
+
+server.listen(4000, () => {
+  console.info('Server is running on http://localhost:4000/graphql');
+});
