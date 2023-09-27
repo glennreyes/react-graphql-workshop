e1a670ff99309f86478318f98bb4c427dcc0f1ab
Glenn Reyes <glenn@glennreyes.com>
2023-09-27 20:28:02 +0200

    Run typecheck

--- app/components/ui/dialog.tsx
+++ app/components/ui/dialog.tsx
@@ -9,9 +9,7 @@ const Dialog = DialogPrimitive.Root;
 
 const DialogTrigger = DialogPrimitive.Trigger;
 
-const DialogPortal = ({ className, ...props }: DialogPrimitive.DialogPortalProps) => (
-  <DialogPrimitive.Portal className={cn(className)} {...props} />
-);
+const DialogPortal = (props: DialogPrimitive.DialogPortalProps) => <DialogPrimitive.Portal {...props} />;
 DialogPortal.displayName = DialogPrimitive.Portal.displayName;
 
 const DialogOverlay = React.forwardRef<
